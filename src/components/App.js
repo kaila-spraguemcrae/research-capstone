3fd6cff007f3c3f3429c4dd53def937e3235470a
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-23 20:50:35 -0800

    add BackDrop and GroundPlane

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Canvas } from 'react-three-fiber';
 import Sphere from './Sphere';
 import Light from './Light';
+import BackDrop from './BackDrop';
+import GroundPlane from './GroundPlane';
 
 function App() {
   return (
     <Canvas>
       <Light brightness={10} color={"white"} />
       <Sphere />
+      <BackDrop />
+      <GroundPlane />
     </Canvas>
   );
 }
