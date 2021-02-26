0e2151f3494a89a1f8726f0c881341cb4bf8e879
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-26 14:04:04 -0800

    add scene files and update SkyObject

--- practice-gltf-render/src/SkyObject.js
+++ practice-gltf-render/src/SkyObject.js
@@ -2,3 +2,15 @@ import React, { useRef } from 'react';
 import { useLoader, useFrame } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
+function SkyObject(){
+  const { scene } = useLoader(GLTFLoader, './scene.gltf');
+  const ref = useRef();
+
+  return (
+    <group ref={ref} position={[0, 0, -100]}>
+      <primitive scale={[0.001, 0.001, 0.001]} object={scene} dispose={null} />
+    </group>
+  );
+}
+
+export default SkyObject;
