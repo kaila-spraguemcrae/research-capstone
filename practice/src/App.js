8b8ba59c47180232c572824026fb18ff8d0db632
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-26 15:59:42 -0800

    add shadows to cubes

--- practice/src/App.js
+++ practice/src/App.js
@@ -13,7 +13,7 @@ const SpinningMesh = ({position, args, color}) => {
   const mesh= useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh castShadow position={position} ref={mesh}>
       <boxBufferGeometry attach='geometry' args={args} />
       <meshStandardMaterial attach='material' color={color} />
     </mesh>
@@ -23,8 +23,33 @@ const SpinningMesh = ({position, args, color}) => {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+      <Canvas shadowMap colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
         <ambientLight intensity={0.3} />
+        <directionalLight 
+          castShadow
+          position={[0,10,0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -12]} intensity={0.5}/>
+        <pointLight position={[0, -10, 0]} intensity={1.5}/>
+
+        <group>
+          <mesh 
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]} 
+            position={[0, -3, 0]}>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <shadowMaterial attach='material' opacity={0.3} />
+          </mesh>
+        </group>
+
         <SpinningMesh position={[0,1,0]} args={[3, 2, 1]} color='pink'/>
         <SpinningMesh position={[-2,1,-5]} color='lightblue'/>
         <SpinningMesh position={[5,1,-2]} color='lavender'/>
