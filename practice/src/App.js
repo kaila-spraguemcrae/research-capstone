cd9d24b85f77672ae6f6e3f0bf368c7bf4b3ddd4
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-26 14:58:19 -0800

    update box to spin

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.scss';
 
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
 
 //NO HTML inside Canvas (only can take three.js elements)
 //mesh can take 'meterial' and 'geomentry'
-//args: [x, y ,z] (size of object: height, width, depth)
+//args: for square:[x, y ,z] (size of object: height, width, depth) // for circle: [1, 1] (size, amount of angles)
+
+const Box = () => {
+  const mesh= useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <>
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-          <meshStandardMaterial attach='material' />
-        </mesh>
+        <Box />
       </Canvas>
     </>
   );
