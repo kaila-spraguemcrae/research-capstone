fceed0c2bc77db8cedcf8ed97d0e1cc6d5a3ab26
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-28 11:40:37 -0800

    add orbit controls and lights

--- practice-gltf-render/src/App.js
+++ practice-gltf-render/src/App.js
@@ -3,6 +3,8 @@ import { Canvas } from 'react-three-fiber';
 // import SkyObject from './SkyObject';
 import './App.css';
 import Model from './components/Three/Scene';
+import Lights from './components/Three/Lights';
+import { OrbitControls } from '@react-three/drei';
 
 // function Sky(){
 //   return (
@@ -19,9 +21,11 @@ function App() {
         colorManagement
         shadowMap
         camera={{position: [-5, 4, 4], fov: 40}}>
+          <Lights/>
           <Suspense fallback={null}>
             <Model />
           </Suspense>
+          <OrbitControls/>
       </Canvas>
     </>
   );
